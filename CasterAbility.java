afd782c4e7de38ce934e0f02673afdca7a24915a
BLARK <vince.pacilan@student.passerellesnumeriques.org>
2026-04-28 22:27:18 +0800

    3rd commit create interface CasterAbility with methods castSpell(String spellName) and getMana()

---
+++ CasterAbility.java
@@ -0,0 +1,16 @@
+/**
+ * Interface for characters that can cast spells.
+ */
+public interface CasterAbility {
+    /**
+     * Cast a spell.
+     * @param spellName the name of the spell to cast
+     */
+    void castSpell(String spellName);
+
+    /**
+     * Get the mana points of the character.
+     * @return the mana points as an int
+     */
+    int getMana();
+}
